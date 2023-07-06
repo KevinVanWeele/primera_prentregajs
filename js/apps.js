2ce40c14d425c934d60828eee0d4fb31bc8cb994
kevin van welle <kevinvanweele09@gmail.com>
2023-07-06 18:38:14 -0300

    agregamos nueva funciones

--- js/apps.js
+++ js/apps.js
@@ -1,169 +1,120 @@
+class BaseDeDatos {
+  constructor() {
+    // Array de la base de datos
+    this.productos = [];
+    // Vamos a cargar todos los productos que tengamos
+    // Con una simple línea de código, vamos a ir cargando todos los productos que tengamos
+    this.agregarRegistro(1, "Chaleco", 100, "Alimentos", "chaleconike.jpg");
+    this.agregarRegistro(2, "Remera", 50, "Alimentos", "remeranike.jpg");
+    this.agregarRegistro(3, "Buzo", 25, "Alimentos", "buzonike.jpg");
+    this.agregarRegistro(4, "Camera", 25, "Alimentos", "camperanike.jpg");
+  }
+
+  // Método que crea el objeto producto y lo almacena en el array con un push
+  agregarRegistro(id, nombre, precio, categoria, imagen) {
+    const producto = new Producto(id, nombre, precio, categoria, imagen);
+    this.productos.push(producto);
+  }
+
+
+traerRegistros() {
+  return this.productos;
+}
+
+registroPorid(id) {
+  return this,productos.find((producto) => producto.id == id);
+}
+
+}
+
 class Carrito {
-    constructor() {
-      this.productos = [];
-      this.total = 0;
-    }
-  
-    enCarrito(nuevoProducto) {
-      return this.productos.find((producto) => {
-        if (producto.nombre == nuevoProducto.nombre && producto.precio == nuevoProducto.precio) {
-          return true;
-        }
-        return false;
-      });
-    }
-  
-    
-    agregar(nuevoProducto) {
-    
-      let productoEncontrado = this.enCarrito(nuevoProducto);
-      if (productoEncontrado) {
-    
-        productoEncontrado.cantidad += 1;
-        productoEncontrado.precio = nuevoProducto.precio;
-        productoEncontrado.subtotal = nuevoProducto.precio * productoEncontrado.cantidad;
-  
-        
-        let cantidad = document.querySelector("#" + productoEncontrado.nombre.toLowerCase());
-        cantidad.innerText = productoEncontrado.cantidad;
-      } else {
-        
-        this.productos.push(nuevoProducto);
-        alert("El producto " + nuevoProducto.nombre + " fue agregado al carrito.");
-  
-        
-        console.log(elementoCarrito);
-        elementoCarrito.innerHTML += `
-          <h1>${nuevoProducto.nombre}</h1>
-          <p>Cantidad: <span id="${nuevoProducto.nombre.toLowerCase()}">${
-          nuevoProducto.cantidad
-        }</span></p>
-          <p>Precio: $${nuevoProducto.precio}</p>
-          <p>Subtotal: $${nuevoProducto.subtotal}</p>
-        `;
-      }
+  constructor() {
+this.carrito = [];
+this.total = 0;
+this.totalProductos = 0;
 
-      this.listar();
-    }
-  
-  
-    listar() {
-      console.clear();
-      console.log("Mis productos en el carrito:");
+}
+
+esasEnCarrtito({ id }){
+  return this.carrito.find(producto => producto.id == id);
+}
+
+agregar(producto){
+  let productoEnCarrito = this.estaEnCarrito(producto);
+  if (productoEnCarrito){
+    productoEnCarrito.cantidad++;
+  } else {
+      this.carrito.push({...producto, cantidad: 1 });
+  }
+}
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+class Producto {
+  constructor(id ,nombre, precio, categoria, imagen = false) {
+    this. id = id;
+    this. nombre = nombre;
+    this. precio = precio;
+    this. categoria = categoria;
+    this. imagen = imagen;
+  }
+}
+
+const bd = new BaseDeDatos();
+
+const divProductos = document.querySelector("#productos");
+
+
+cargarProductos();
+
+
+
+function cargarProductos(){
+  const productos = bd.traerRegistros();
+  divProductos,innerHTML = "";
+
+  for (const producto of productos) {
+    divProductos.innerHTML += `
+   <div class="producto">
+   <h2>${producto.nombre}</h2>
+   <p>${producto.precio}</p>
+   <img src="img/${producto.imagen}" />
+   <p><a href="#" class="btnAgregar" data-id="${producto.id}">Agregar al carrito</a></p>
    
-      this.productos.forEach((producto) => {
-        console.log("------");
-        console.log("Nombre: " + producto.nombre);
-        console.log("Precio: " + producto.precio);
-        console.log("Cantidad: " + producto.cantidad);
-        console.log("Subtotal: " + producto.subtotal);
-      });
-  
-
-      this.total = this.productos.reduce(
-        (acumulador, producto) => acumulador + producto.precio * producto.cantidad,
-        0
-      );
-      console.log("------");
-      console.log("TOTAL DEl CARRITO: $" + this.total);
-    }
-  
-
-    quitar(producto) {
-
-      let productoEncontrado = this.enCarrito(producto);
-      if (productoEncontrado) {
-     
-        let indice = this.productos.indexOf(productoEncontrado);
-        this.productos.splice(indice, 1);
-        alert("El producto " + producto.nombre + " fue borrado del carrito");
-        this.listar();
-      }
-    }
-  
-
-    buscar(nombreProducto) {
-
-      let resultado = this.productos.filter((producto) =>
-
-        producto.nombre.includes(nombreProducto)
-      );
-
-      console.clear();
-      console.log("Productos encontrados:");
-      resultado.forEach((producto) => {
-        console.log("------");
-        console.log("Nombre: " + producto.nombre);
-        console.log("Precio: " + producto.precio);
-        console.log("Cantidad: " + producto.cantidad);
-        console.log("Subtotal: " + producto.subtotal);
-      });
-    }
+   
+   </div>
+   
+   
+    `;
   }
-  
 
-  const elementoCarrito = document.querySelector("#carrito");
-  const btnAgregar = document.querySelector("#btnAgregar");
-  const btnQuitar = document.querySelector("#btnQuitar");
-  const btnListar = document.querySelector("#btnListar");
-  const btnBuscar = document.querySelector("#btnBuscar");
-  
-
-  const carrito = new Carrito();
-  
-  btnAgregar.addEventListener("click", agregarProducto);
-  btnQuitar.addEventListener("click", quitarProducto);
-  btnListar.addEventListener("click", function () {
-    carrito.listar();
+const botonesAgregar = document.querySelectorAll(".btnAgregar");
+for (const boton of botonesAgregar){
+  boton.addEventListener("clikc" , (event) => {
+    event.preventDefault();
+    const id = Number(boton.dataset.id);
+    const producto = bd.registroPorid(id);
+    console.log("Esta agregando el producto:" , producto.nombre);
   });
-  btnBuscar.addEventListener("click", buscarProducto);
-  
+}
 
-  
-  function agregarProducto() {
-
-  
-
-    let nombre = prompt("Introduzca el nombre del producto");
-
-    if (nombre == "") {
-      alert("El nombre ingresado es inválido, ingrese los datos de nuevo.");
-      return;
-    }
-  
-
-    let precio = prompt("Introduzca el precio del producto");
-
-    if (precio == "" || parseInt(precio) <= 0) {
-      alert("El precio ingresado es inválido, ingrese los datos de nuevo.");
-      return;
-    }
-  
-
-    const nuevoProducto = {
-      nombre: nombre,
-      precio: parseInt(precio),
-      cantidad: 1,
-      subtotal: parseInt(precio),
-    };
-  
-
-    carrito.agregar(nuevoProducto);
-  }
-  
-  function quitarProducto() {
-
-    let nombre = prompt("Introduzca el nombre del producto que desea quitar");
-    let precio = prompt("Introduzca el precio del producto que desea quitar");
+}
 
 
-    carrito.quitar({ nombre: nombre, precio: precio });
-  }
-  
-  function buscarProducto() {
+const carrito = new Carrito();
 
-    let nombre = prompt("Introduzca el nombre del producto que desea buscar");
-  
-
-    carrito.buscar(nombre);
-  }
